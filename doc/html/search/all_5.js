824013894872a128ff8995a88c529336cb71db90
eliott naigre <esn@esn-debian.home>
2024-12-15 14:57:24 +0000

    Nothing, really

--- doc/html/search/all_5.js
+++ doc/html/search/all_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['mesh3d_0',['Mesh3D',['../classMesh3D.html',1,'']]],
-  ['moveable_1',['Moveable',['../classMoveable.html',1,'']]]
+  ['mesh3d_0',['Mesh3D',['../classMesh3D.html',1,'']]]
 ];
